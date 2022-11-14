6352167fedd3b5c65f72cffab9c250a332a7e1af
Regina Iulgutlina <reginaiulgut@gmail.com>
2022-11-14 13:15:50 -0600

    added homework08

--- src/homeworks/Homework08.java
+++ src/homeworks/Homework08.java
@@ -1,6 +1,7 @@
 package homeworks;
 
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
@@ -13,10 +14,14 @@ public class Homework08 {
 
         System.out.println("\n-------TEST TASK-2-------\n");
         String str2 = "Java  is   fun  ";
+        System.out.println(Arrays.toString(wordArray(str2)));
 
         System.out.println("\n-------TEST TASK-3-------\n");
         String str3 = "Java  is   fun  ";
         System.out.println(removeExtraSpaces(str3));
+
+        System.out.println("\n-------TEST TASK-4-------\n");
+        System.out.println(count3OrLess());
 
         System.out.println("\n-------TEST TASK-5-------\n");
         String str5 = "10/02/1991";
@@ -26,24 +31,32 @@ public class Homework08 {
         String str6 = "abc@gmail.com";
         System.out.println(isEmailFormatValid(str6));
 
-
-
     }
 
     public static int countConsonants(String str){
         return str.replaceAll("[aeiouAEIOU]", "").length();
     }
 
-    /*
-    public static String[] wordArray (String word){
-        String[] wordArray = word.split(" ");
-        Arrays.stream(wordArray).filter(x -> x.equals(" "));
+    public static String[] wordArray(String str) {
+        str = str.replaceAll("\\s+", " ").trim();
+        String[] wordArray = str.split(" ");
+        return wordArray;
     }
-
-     */
 
     public static String removeExtraSpaces(String str){
         return str.replaceAll("\\s+", " ");
+    }
+
+    public static int count3OrLess(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a sentence");
+        String str = scanner.nextLine();
+        String[] arr = str.split(" ");
+        int count = 0;
+        for (String s : arr) {
+            if (s.length() <= 3) count++;
+        }
+        return count;
     }
 
     public static boolean isDateFormatValid(String dateOfBirth){
